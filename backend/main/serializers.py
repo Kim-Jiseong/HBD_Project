c8cd6f0ddb59c5ddcccb8a2aa7a21b3aa82068a3
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-11-20 18:26:08 +0900

    serializers

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Photo, Letter
 
 
-class PhotoSerializer(serializers.ModelSerializer):
-    img = serializers.ImageField(use_url=True)
+class PhotoSerializer(serializers.HyperlinkedSerializer):
+    # img = serializers.ImageField(use_url=True)
     class Meta:        
         model = Photo
         fields = "__all__"
 
-class LetterSerializer(serializers.ModelSerializer):
+class LetterSerializer(serializers.HyperlinkedSerializer):
     class Meta:
         model = Letter
         fields = "__all__"
