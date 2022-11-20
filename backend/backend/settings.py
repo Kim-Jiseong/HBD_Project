5850930ca996dc3f13356ca366584c565b295402
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-11-20 18:22:54 +0900

    serializers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
     'http://localhost:3000',
     'http://*.ap-northeast-2.compute.amazonaws.com',
     # '*',
