1e51dba19905eda77816478b9097d1b59996818e
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-11-20 16:48:07 +0900

    deploy url setting

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include("main.urls")),
+    path("api/", include("rest_framework.urls", namespace="rest_framework")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
